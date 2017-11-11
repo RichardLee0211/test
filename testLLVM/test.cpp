a28ef76e14e4ddfebff549f7c8b41537c1bcbabe
Richard@MacbookAir <richardlee0211er@gmail.com>
2017-11-11 10:07:59 -0500

    add testLLVM

---
+++ testLLVM/test.cpp
@@ -0,0 +1,12 @@
+/**
+ * following http://releases.llvm.org/2.6/docs/tutorial/JITTutorial1.html
+ */
+#include<iostream>
+
+#include "llvm/Module.h"
+
+
+int main(){
+    std::cout<<"hello world\n";
+    return 0;
+}
