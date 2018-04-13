09c421f438f6abbf9f91257f7a30de8310102f37
Richard@MacbookAir <richardlee0211er@gmail.com>
2018-04-13 14:09:26 -0400

    little change of testgdb

--- testC/gdb/test.c
+++ testC/gdb/test.c
@@ -1,18 +1,19 @@
+/* compile with -pthread */
 #include<stdio.h>
 #include<pthread.h>
 
 void *thread_func(void *p_arg){
     while(1){
         printf("%s\n", (char*)p_arg);
-        sleep(10);
+        sleep(2);
     }
 }
 
 int main(void){
     pthread_t t1, t2;
 
-    // pthread_create(&t1, NULL, thread_func, "Thread 1");
-    // pthread_create(&t2, NULL, thread_func, "Thread 2");
+    pthread_create(&t1, NULL, thread_func, "Thread 1");
+    pthread_create(&t2, NULL, thread_func, "Thread 2");
 
     while(1){
         printf("delay\n");
